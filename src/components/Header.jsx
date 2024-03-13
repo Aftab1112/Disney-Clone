20ddfc8719af92b95ebcf816c765530490e55044
Aftab1112 <you@example.com>
2024-03-13 07:12:18 +0530

    made header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./../assets/Images/logo.png";
 import { HiHome, HiMagnifyingGlass, HiStar } from "react-icons/hi2";
 import { HiPlus, HiDotsVertical } from "react-icons/hi";
@@ -7,6 +7,8 @@ import { RiMovie2Fill } from "react-icons/ri";
 import HeaderItem from "./HeaderItem";
 
 function Header() {
+  const [toggle, setToggle] = useState(false);
+
   const menu = [
     {
       name: "HOME",
@@ -37,10 +39,31 @@ function Header() {
   return (
     <div className="flex items-center  justify-between p-5">
       <div className="flex gap-8 items-center">
-        <img className="w-[80px] md:w-[115px] object-cover" src={logo} />
-        {menu.map((item, i) => (
-          <HeaderItem name={item.name} Icon={item.icon} key={i} />
-        ))}
+        <img className="w-[80px] md:w-[115px] object-cover mb-5" src={logo} />
+        <div className="hidden md:flex gap-8">
+          {menu.map((item, i) => (
+            <HeaderItem name={item.name} Icon={item.icon} key={i} />
+          ))}
+        </div>
+        <div className="flex md:hidden gap-5">
+          {menu.map(
+            (item, i) =>
+              i < 3 && <HeaderItem name={""} Icon={item.icon} key={i} />
+          )}
+          <div className="md:hidden " onClick={() => setToggle(!toggle)}>
+            <HeaderItem name={""} Icon={HiDotsVertical} />
+            {toggle ? (
+              <div className="absolute mt-3 bg-[#121212] b-[1px] p-3 border-gray-700 px-5 py-4">
+                {menu.map(
+                  (item, i) =>
+                    i > 2 && (
+                      <HeaderItem name={item.name} Icon={item.icon} key={i} />
+                    )
+                )}
+              </div>
+            ) : null}
+          </div>
+        </div>
       </div>
       <img
         className="w-[40px] rounded-full"
